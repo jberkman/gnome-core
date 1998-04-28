a28c6b5ea42ea1b8b68dfc35bc4b5802f06036a9
Arturo Espinosa <unammx@src.gnome.org>
1998-04-28 23:44:43 +0000

    Added property page, it is still not finished. I do not know if I would
    Added property page, it is still not finished.  I do not know
    if I would like to have the properties applied to all of the windows
    or the properties applied only to the terminal where the property
    page was invoked.
    
    Another option is a checkbutton: "Global apply changes" in the
    property page.  Ideas on this issue are welcome.
    
    Miguel.
    
    svn path=/trunk/; revision=716

--- gnome-terminal/gnome-terminal.c
+++ gnome-terminal/gnome-terminal.c
@@ -17,7 +17,7 @@ char **env;
 #define EXTRA 2
 
 /* Font used by the terminals */
-char *font;
+char *font = NULL;
 
 /* Number of scrollbacklines */
 int scrollback;
@@ -30,13 +30,19 @@ enum {
 /* How to invoke the shell */
 int invoke_as_login_shell = 1;
 
+/* Do we want blinking cursor? */
+int blink;
+
 /* A list of all the open terminals */
 GList *terminals = 0;
+
+/* The property window  and the font window */
+GtkWidget *prop_win = 0, *font_button;
 
 void new_terminal (void);
 
 static void
-about_terminal_cmd (GtkWidget *widget, void *data)
+about_terminal_cmd (void)
 {
         GtkWidget *about;
 
@@ -55,33 +61,106 @@ about_terminal_cmd (GtkWidget *widget, void *data)
 }
 
 static void
-close_terminal_cmd (GtkWidget *widget, void *data)
+close_terminal_cmd (void *unused, void *data)
 {
 	terminals = g_list_remove (terminals, data);
 	gtk_widget_destroy (GTK_WIDGET (data));
+	if (terminals == NULL)
+		gtk_main_quit ();
 }
 
 static void
-close_all_cmd (GtkWidget *widget)
+close_all_cmd (void)
 {
 	while (terminals)
-		close_terminal_cmd (widget, terminals->data);
-	gtk_main_quit ();
+		close_terminal_cmd (0, terminals->data);
+}
+
+static GtkWidget *
+aligned_label (char *str)
+{
+	GtkWidget *l;
+
+	l = gtk_label_new (str);
+	gtk_misc_set_alignment (GTK_MISC (l), 1.0, 0.5);
+	return l;
+}
+
+static void
+apply_changes (GtkWidget *widget, int page, ZvtTerm *term)
+{
+	zvt_term_set_font_name (term, GTK_LABEL (GTK_BUTTON (font_button)->child)->label);
+}
+
+static int
+window_closed (void)
+{
+	prop_win = 0;
+	return FALSE;
+}
+
+static void
+choose_font (void)
+{
+	char *font;
+	GtkWidget *l;
+	
+	font = gnome_font_select ();
+	if (strcmp (font, GTK_LABEL(GTK_BUTTON (font_button)->child)->label) == 0)
+		return;
+	
+	gtk_container_remove (GTK_CONTAINER (font_button), GTK_BUTTON (font_button)->child);
+	l = gtk_label_new (font);
+	gtk_widget_show (l);
+	gtk_container_add (GTK_CONTAINER (font_button), l);
+	gnome_property_box_changed (prop_win);
+}
+
+static void
+preferences_cmd (GtkWidget *widget, ZvtTerm *term)
+{
+	GtkTable *table;
+	GtkWidget *l, *e;
+	
+	if (prop_win)
+		return;
+
+	prop_win = gnome_property_box_new ();
+
+	/* Look page */
+	table = gtk_table_new (0, 0, 0);
+	gnome_property_box_append_page (GNOME_PROPERTY_BOX (prop_win),
+					table, gtk_label_new (_("Look")));
+	l = aligned_label (_("Color scheme:"));
+	gtk_table_attach (GTK_TABLE (table), l,
+			  1, 2, 1, 2, GTK_FILL, 0, GNOME_PAD, GNOME_PAD);
+	l = aligned_label (_("Font:"));
+	gtk_table_attach (GTK_TABLE (table), l,
+			  1, 2, 2, 3, GTK_FILL, 0, GNOME_PAD, GNOME_PAD);
+	font_button = gtk_button_new_with_label (font);
+	gtk_signal_connect (GTK_OBJECT (font_button), "clicked", GTK_SIGNAL_FUNC(choose_font), NULL);
+	gtk_table_attach (GTK_TABLE (table), font_button,
+			  2, 3, 2, 3, GTK_FILL, 0, GNOME_PAD, GNOME_PAD);
+	l = aligned_label (_("Scrollbar position"));
+	gtk_table_attach (GTK_TABLE (table), l,
+			  1, 2, 3, 4, GTK_FILL, 0, GNOME_PAD, GNOME_PAD);
+
+	gtk_signal_connect (GTK_OBJECT (prop_win), "apply",
+			    GTK_SIGNAL_FUNC (apply_changes), term);
+	gtk_signal_connect (GTK_OBJECT (prop_win), "delete_event",
+			    GTK_SIGNAL_FUNC (window_closed), term);
+	gtk_signal_connect (GTK_OBJECT (prop_win), "destroy",
+			    GTK_SIGNAL_FUNC (window_closed), term);
+	gtk_widget_show_all (prop_win);
 }
 
 static GnomeUIInfo gnome_terminal_terminal_menu [] = {
-	{ GNOME_APP_UI_ITEM, N_("New terminal"),  NULL, new_terminal },
+	{ GNOME_APP_UI_ITEM, N_("New terminal"),    NULL, new_terminal },
 	{ GNOME_APP_UI_ITEM, N_("Close terminal"),  NULL, close_terminal_cmd },
+	{ GNOME_APP_UI_ITEM, N_("Properties..."),   NULL, preferences_cmd, 0, 0,
+	  GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_PROP },
 	{ GNOME_APP_UI_SEPARATOR },
 	{ GNOME_APP_UI_ITEM, N_("Close all terminals"),  NULL, close_all_cmd },
-	GNOMEUIINFO_END
-};
-
-static GnomeUIInfo gnome_terminal_options_menu [] = {
-	{ GNOME_APP_UI_ITEM, N_("Hide menubar"),        NULL, about_terminal_cmd },
-	{ GNOME_APP_UI_ITEM, N_("Secure keyboard"),     NULL, about_terminal_cmd },
-	{ GNOME_APP_UI_ITEM, N_("Select font"),         NULL, about_terminal_cmd },
-	{ GNOME_APP_UI_ITEM, N_("Color set"),           NULL, about_terminal_cmd },
 	GNOMEUIINFO_END
 };
 
@@ -95,7 +174,6 @@ static GnomeUIInfo gnome_terminal_about_menu [] = {
 
 static GnomeUIInfo gnome_terminal_menu [] = {
 	{ GNOME_APP_UI_SUBTREE, N_("Terminal"), NULL, &gnome_terminal_terminal_menu },
-	{ GNOME_APP_UI_SUBTREE, N_("Options"), NULL,  &gnome_terminal_options_menu },
 	{ GNOME_APP_UI_SUBTREE, N_("Help"), NULL,     &gnome_terminal_about_menu },
 	GNOMEUIINFO_END
 };
@@ -136,9 +214,9 @@ get_shell_name (char **shell, char **name)
 void
 terminal_kill (GtkWidget *widget, void *data)
 {
-	GnomeApp *app = GNOME_APP (data);
-	
-	gtk_widget_destroy (GTK_WIDGET (app));
+	GnomeApp *app = GNOME_APP (gtk_widget_get_toplevel (GTK_WIDGET (data)));
+
+	close_terminal_cmd (widget, app);
 }
 
 void
@@ -177,17 +255,19 @@ new_terminal (void)
 	}
 
 	app = gnome_app_new ("GnomeTerminal", "Terminal");
-	gnome_app_create_menus_with_data (GNOME_APP (app), gnome_terminal_menu, app);
 	gtk_window_set_wmclass (GTK_WINDOW (app), "GnomeTerminal", "GnomeTerminal");
 	gtk_widget_realize (app);
 	terminals = g_list_prepend (terminals, app);
-	
+
 	/* Setup the Zvt widget */
 	term = ZVT_TERM (zvt_term_new ());
 	zvt_term_set_scrollback (term, scrollback);
 	zvt_term_set_font_name  (term, font);
+	zvt_term_set_blink (term, blink);
 	gtk_signal_connect (GTK_OBJECT (term), "child_died",
-			    GTK_SIGNAL_FUNC (terminal_kill), app);
+			    GTK_SIGNAL_FUNC (terminal_kill), term);
+	
+	gnome_app_create_menus_with_data (GNOME_APP (app), gnome_terminal_menu, term);
 	
 	/* Decorations */
 	hbox = gtk_hbox_new (0, 0);
@@ -224,25 +304,29 @@ static void
 terminal_load_defaults (void)
 {
 	char *p;
-	
+
 	scrollback = gnome_config_get_int    ("/Terminal/Config/scrollbacklines=100");
-	font       = gnome_config_get_string ("/Terminal/Config/font=" DEFAULT_FONT);
-	p          = gnome_config_get_string ("/Terminal/Config/scrollpos=left");
+	if (!font)
+		font       = gnome_config_get_string ("/Terminal/Config/font=" DEFAULT_FONT);
+	p = gnome_config_get_string ("/Terminal/Config/scrollpos=left");
 	if (strcasecmp (p, "left") == 0)
 		scrollbar_position = SCROLLBAR_LEFT;
 	else if (strcasecmp (p, "right") == 0)
 		scrollbar_position = SCROLLBAR_RIGHT;
 	else
 		scrollbar_position = SCROLLBAR_HIDDEN;
+	blink = gnome_config_get_bool ("/Terminal/Config/blinking=0");
 }
 
 /* Keys for the ARGP parser, should be negative */
 enum {
-	FONT_KEY = -1
+	FONT_KEY = -1,
+	NOLOGIN_KEY = -2
 };
 
 static struct argp_option argp_options [] = {
-	{ "font",  FONT_KEY, N_("FONT"), 0, N_("Specifies font name"),                  0 },
+	{ "font",    FONT_KEY,    N_("FONT"), 0, N_("Specifies font name"),                    0 },
+	{ "nologin", NOLOGIN_KEY, NULL,       0, N_("Do not start up shells as login shells"), 0 },
 	{ NULL, 0, NULL, 0, NULL, 0 },
 };
 
@@ -253,7 +337,9 @@ parse_an_arg (int key, char *arg, struct argp_state *state)
 	case FONT_KEY:
 		font = arg;
 		break;
-		
+	case NOLOGIN_KEY:
+	        invoke_as_login_shell = 0;
+	        break;
 	default:
 		return ARGP_ERR_UNKNOWN;
 	}
@@ -274,6 +360,7 @@ main (int argc, char *argv [], char **environ)
 	
 	bindtextdomain (PACKAGE, GNOMELOCALEDIR);
 	textdomain (PACKAGE);
+	
 	gnome_init ("Terminal", &parser, argc, argv, 0, NULL);
 
 	terminal_load_defaults ();
