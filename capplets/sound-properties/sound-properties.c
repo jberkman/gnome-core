7500b97e6962b1acabe2549d19526ff8172603e0
Elliot Lee <sopwith@src.gnome.org>
1998-10-20 21:09:59 +0000

    sound properties functionality is there; now it just need cappletizing
    sound properties functionality is there; now it just need cappletizing
    (the infrastructure needed to cappletize it is already there, just needs
    hooking in).
    
    svn path=/trunk/; revision=1914

--- capplets/sound-properties/sound-properties.c
+++ capplets/sound-properties/sound-properties.c
@@ -8,6 +8,8 @@
 #include <config.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <gtk/gtk.h>
 #include "capplet-widget.h"
 
@@ -20,8 +22,12 @@ typedef struct {
     GtkWidget *enable_esd_startup, *enable_sound_events;
 
     /* Sound events setup */
-    GtkWidget *ctree;
+    GtkWidget *table, *ctree;
     GHashTable *entries;
+
+    GtkWidget *btn_filename, *btn_play;
+
+    gint ignore_changed;
 } SoundProps;
 
 typedef struct {
@@ -37,6 +43,16 @@ static void sound_properties_event_free(SoundEvent *ev);
 static void sound_properties_read_path(SoundProps *props,
                                        GString *tmpstr,
                                        const char *path);
+static void sound_properties_event_select(GtkCTree *ctree,
+                                          GtkCTreeNode *row,
+                                          gint column,
+                                          SoundProps *props);
+static void sound_properties_event_change_file(GtkEditable *entry,
+                                               SoundProps *props);
+static void sound_properties_set_sensitivity(GtkToggleButton *btn,
+                                             SoundProps *props);
+static void sound_properties_event_apply(GtkCTreeNode *node, SoundProps *props); 
+static void sound_properties_apply(SoundProps *props); 
 
 int
 main(int argc,
@@ -45,9 +61,9 @@ main(int argc,
     SoundProps *sound_properties;
 
 #ifdef TESTING
-    gnome_init ("sound-properties", NULL, argc, argv, 0, NULL);
+    gnome_init ("sound-properties", VERSION, argc, argv);
 #else
-    gnome_capplet_init ("sound-properties", NULL, argc, argv, 0, NULL);
+    gnome_capplet_init ("sound-properties", VERSION, argc, argv, NULL, 0, NULL);
 #endif
 
     sound_properties = sound_properties_create();
@@ -74,11 +90,15 @@ sound_properties_create(void)
         "Event",
         "File to Play"
     };
+    gboolean btmp;
 
     retval = g_new0(SoundProps, 1);
 
 #ifdef TESTING
-    retval->capplet = gtk_window_new(GTK_WINDOW_TOPLEVEL);    
+    retval->capplet = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_policy(GTK_WINDOW(retval->capplet), TRUE, TRUE, TRUE);
+    gtk_signal_connect(GTK_OBJECT(retval->capplet), "delete_event",
+                       GTK_SIGNAL_FUNC(gtk_main_quit), NULL);
 #else
     retval->capplet = capplet_widget_new();
 #endif
@@ -92,30 +112,70 @@ sound_properties_create(void)
                       (retval->enable_esd_startup =
                        gtk_check_button_new_with_label("Enable GNOME sound support")));
 
+    gtk_toggle_button_set_state(GTK_TOGGLE_BUTTON(retval->enable_esd_startup),
+                                gnome_config_get_bool("/sound/system/settings/start_esd"));
+
     gtk_container_add(GTK_CONTAINER(wtmp),
                       (retval->enable_sound_events =
                        gtk_check_button_new_with_label("Enable sounds for events")));
 
-    gtk_widget_set_sensitive(retval->enable_esd_startup, FALSE);
-    gtk_widget_set_sensitive(retval->enable_sound_events, FALSE);
+    gtk_signal_connect(GTK_OBJECT(retval->enable_esd_startup),
+                       "toggled",
+                       GTK_SIGNAL_FUNC(sound_properties_set_sensitivity),
+                       retval);
+
+    gtk_signal_connect(GTK_OBJECT(retval->enable_sound_events),
+                       "toggled",
+                       GTK_SIGNAL_FUNC(sound_properties_set_sensitivity),
+                       retval);
+
+    gtk_toggle_button_set_state(GTK_TOGGLE_BUTTON(retval->enable_sound_events),
+                                gnome_config_get_bool("/sound/system/settings/event_sounds"));
 
     gtk_notebook_append_page(GTK_NOTEBOOK(notebook), wtmp,
                              gtk_label_new("General"));
 
     /* * * * page "Sound Events" * * * */
-    table = gtk_table_new(3, 2, FALSE);
+    retval->table = table = gtk_table_new(2, 2, FALSE);
     retval->ctree = gtk_ctree_new_with_titles(3, 0,
                                               (gchar **)ctree_column_titles);
+    gtk_signal_connect(GTK_OBJECT(retval->ctree), "tree_select_row",
+                       sound_properties_event_select, retval);
+
+    gtk_clist_set_selection_mode(GTK_CLIST(retval->ctree), GTK_SELECTION_BROWSE);
+
+    gtk_clist_set_column_width(GTK_CLIST(retval->ctree), 0, 100);
+    gtk_clist_set_column_width(GTK_CLIST(retval->ctree), 1, 150);
+    gtk_clist_set_column_width(GTK_CLIST(retval->ctree), 2, 200);
+    gtk_widget_set_usize(GTK_WIDGET(retval->ctree), 100+150+200+20,
+                         200);
+
+    gtk_ctree_set_expander_style(GTK_CTREE(retval->ctree),
+                                 GTK_CTREE_EXPANDER_SQUARE);
 
     sound_properties_regenerate_ctree(retval);
 
-    gtk_table_attach_defaults(GTK_TABLE(table), retval->ctree, 0, 1, 0, 3);
+    gtk_container_border_width(GTK_CONTAINER(table), GNOME_PAD_SMALL);
+    gtk_table_attach_defaults(GTK_TABLE(table), retval->ctree, 0, 2, 0, 1);
+    gtk_table_attach_defaults(GTK_TABLE(table),
+                              (retval->btn_play = gtk_button_new_with_label("Play")),
+                              0, 1, 1, 2);
+    gtk_table_attach_defaults(GTK_TABLE(table),
+                              (retval->btn_filename = gnome_file_entry_new(NULL, "Select sound file")),
+                              1, 2, 1, 2);
+    gtk_signal_connect(GTK_OBJECT(gnome_file_entry_gtk_entry(GNOME_FILE_ENTRY(retval->btn_filename))),
+                       "changed",
+                       GTK_SIGNAL_FUNC(sound_properties_event_change_file),
+                       retval);
+
     gtk_notebook_append_page(GTK_NOTEBOOK(notebook), table,
                              gtk_label_new("Sound Events"));
 
     /* * * * end notebook page setup * * * */
 
     gtk_container_add(GTK_CONTAINER(retval->capplet), notebook);
+
+    sound_properties_set_sensitivity(NULL, retval);
 
     gtk_widget_show_all(retval->capplet);
 
@@ -135,11 +195,15 @@ static void
 sound_properties_regenerate_ctree(SoundProps *props)
 {
     GString *tmpstr = g_string_new(NULL);
+    char *ctmp, *ctmp2;
 
     gtk_clist_freeze(GTK_CLIST(props->ctree));
     gtk_clist_clear(GTK_CLIST(props->ctree));
 
-    sound_properties_read_path(props, tmpstr, "/sound/apps");
+    sound_properties_read_path(props, tmpstr, GNOMESYSCONFDIR "/sound/apps");
+    ctmp = gnome_util_home_file("sound/apps");
+    sound_properties_read_path(props, tmpstr, ctmp);
+    g_free(ctmp);
 
     g_string_free(tmpstr, TRUE);
     gtk_clist_thaw(GTK_CLIST(props->ctree));
@@ -160,43 +224,47 @@ sound_properties_read_path(SoundProps *props,
                            GString *tmpstr,
                            const char *path)
 {
+    DIR *dirh;
     SoundEvent *new_entry;
     char *category_name, *sample_name, *sample_file, *ctmp;
     gpointer top_iter, event_iter;
-    GtkCTreeNode *category_node, *event_node;
-    char *arow[3] = {NULL,NULL,NULL};
+    GtkCTreeNode *category_node = NULL, *event_node = NULL;
+    char *arow[4] = {NULL,NULL,NULL,NULL};
+    struct dirent *dent;
 
-    top_iter = gnome_config_init_iterator_sections(path);
-    while((top_iter = gnome_config_iterator_next(top_iter,
-                                                 NULL, &category_name))) {
+    dirh = opendir(path);
+    if(!dirh)
+        return;
 
-        g_message("Got category %s", category_name);
-        g_string_sprintf(tmpstr, "%s/%s/events", path, category_name);
+    while((dent = readdir(dirh))) {
+	    if (!strcmp(dent->d_name, ".") || !strcmp(dent->d_name, ".."))
+		    continue;
 
-        arow[0] = category_name; arow[1] = NULL; arow[2] = NULL;
+        g_string_sprintf(tmpstr, "=%s/%s=/events", path, dent->d_name);
+
+        arow[0] = dent->d_name; arow[1] = NULL; arow[2] = NULL;
         category_node = gtk_ctree_insert_node(GTK_CTREE(props->ctree),
                                               NULL, NULL,
                                               arow, GNOME_PAD_SMALL,
                                               NULL, NULL, NULL, NULL, FALSE,
                                               FALSE);
+        gtk_ctree_node_set_selectable(GTK_CTREE(props->ctree), category_node,
+                                      FALSE);
         gnome_config_push_prefix(tmpstr->str);
 
         event_node = NULL;
-        event_iter = gnome_config_init_iterator("/");
+        event_iter = gnome_config_init_iterator(tmpstr->str);
         while((event_iter = gnome_config_iterator_next(event_iter,
                                                        &sample_name,
                                                        &sample_file))) {
-            g_message("Got event %s -> %s in category %s",
-                      sample_name, sample_file,
-                      category_name);
             arow[0] = NULL; arow[1] = sample_name; arow[2] = sample_file;
             event_node = gtk_ctree_insert_node(GTK_CTREE(props->ctree),
-                                               NULL, event_node,
+                                               category_node, event_node,
                                                arow, GNOME_PAD_SMALL,
                                                NULL, NULL, NULL, NULL, TRUE,
                                                FALSE);
             new_entry = g_new0(SoundEvent, 1);
-            new_entry->category = g_strdup(category_name);
+            new_entry->category = g_strdup(dent->d_name);
             new_entry->name = sample_name;
             new_entry->file = sample_file;
             new_entry->row = event_node;
@@ -206,7 +274,90 @@ sound_properties_read_path(SoundProps *props,
         }
 
         gnome_config_pop_prefix();
-
-        g_free(category_name);
     }
+    closedir(dirh);
+}
+
+static void
+sound_properties_event_play(GtkWidget *widget, SoundProps *props)
+{
+    GtkWidget *entry;
+
+    entry = gnome_file_entry_gtk_entry(GNOME_FILE_ENTRY(props->btn_filename));
+
+    gnome_sound_play(gtk_entry_get_text(GTK_ENTRY(entry)));
+}
+
+static void
+sound_properties_event_change_file(GtkEditable *entry, SoundProps *props)
+{
+    if(props->ignore_changed)
+        return;
+
+    g_return_if_fail(GTK_CLIST(props->ctree)->selection);
+
+    gtk_ctree_node_set_text(GTK_CTREE(props->ctree),
+                            GTK_CLIST(props->ctree)->selection->data,
+                            2,
+                            gtk_entry_get_text(GTK_ENTRY(entry)));
+}
+
+static void
+sound_properties_event_select(GtkCTree *ctree,
+                              GtkCTreeNode *row,
+                              gint column,
+                              SoundProps *props)
+{
+    char *ctmp;
+
+    ctmp = GTK_CLIST_ROW(&row->list)->cell[2].u.text;
+    props->ignore_changed++;
+    gtk_entry_set_text(GTK_ENTRY(gnome_file_entry_gtk_entry(GNOME_FILE_ENTRY(props->btn_filename))), ctmp);
+    props->ignore_changed--;
+}
+
+
+static void
+sound_properties_event_apply(GtkCTreeNode *node,
+                             SoundProps *props)
+{
+    SoundEvent *ev;
+    char *cur_filename, *ctmp;
+    GtkWidget *entry;
+
+    entry = gnome_file_entry_gtk_entry(GNOME_FILE_ENTRY(props->btn_filename));
+    cur_filename = gtk_entry_get_text(GTK_ENTRY(entry));
+    ev = gtk_ctree_node_get_row_data(GTK_CTREE(props->ctree), node);
+
+    /* If the user didn't change the setting, no need to set it */
+    if(!strcmp(cur_filename, ev->file))
+        return;
+
+    ctmp = g_copy_strings("/sound/apps/", ev->category, "/events/", ev->name);
+    gnome_config_set_string(ctmp, cur_filename);
+    g_free(ctmp);
+}
+
+static void
+sound_properties_apply(SoundProps *props)
+{
+    g_list_foreach(GTK_CLIST(props->ctree)->row_list,
+                   (GFunc)sound_properties_event_apply, props);
+
+    gnome_config_set_bool("/sound/system/settings/start_esd",
+                          GTK_TOGGLE_BUTTON(props->enable_esd_startup)->active);
+    gnome_config_set_bool("/sound/system/settings/event_sounds",
+                          GTK_TOGGLE_BUTTON(props->enable_sound_events)->active);
+    gnome_config_sync();
+}
+
+static void
+sound_properties_set_sensitivity(GtkToggleButton *btn,
+                                 SoundProps *props)
+{
+    gtk_widget_set_sensitive(props->enable_sound_events,
+                             GTK_TOGGLE_BUTTON(props->enable_esd_startup)->active);
+    gtk_widget_set_sensitive(props->table,
+                             GTK_TOGGLE_BUTTON(props->enable_esd_startup)->active
+                             && GTK_TOGGLE_BUTTON(props->enable_sound_events)->active);
 }
