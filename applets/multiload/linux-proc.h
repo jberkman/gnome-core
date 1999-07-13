036fab55c65e789756678dcefcfc0f29396fb0ba
Jacob Berkman <jberkman@andrew.cmu.edu>
1999-07-13 23:33:37 +0000

    another_clock asclock batmon battery bussign cdplayer charpick clockmail
    1999-07-13  Jacob Berkman  <jberkman@andrew.cmu.edu>
    
    	* removed applets which are in gnome-applets.  These are:
    	another_clock
    	asclock
    	batmon
    	battery
    	bussign
    	cdplayer
    	charpick
    	clockmail
    	cpumemusage
    	diskusage
    	esd-manager
    	fifteen
    	freshapp
    	fvwm-pager
    	gkb
    	gnome-pager
    	mixer
    	modemlights
    	multiload
    	netload
    	netwatch
    	newslashapp (now 'slashapp' in gnome-applets)
    	slashapp (now 'oldslashapp' in gnome-applets)
    	webcontrol
    	winlist
    
    	The applets which remain are:
    
    	desk guide
    	fish
    	gen_util
    	tasklist
    
    svn path=/trunk/; revision=3614

--- applets/multiload/linux-proc.h
+++
@@ -1,9 +0,0 @@
-#ifndef LINUX_PROC_H__
-#define LINUX_PROC_H__
-
-void GetLoad (int Maximum, int data [4]);
-void GetMemory (int Maximum, int data [4]);
-void GetSwap (int Maximum, int data [2]);
-void GetNet (int Maximum, int data [3]);
-
-#endif
