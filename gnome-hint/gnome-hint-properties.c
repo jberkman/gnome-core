8987d53d4d6ee52fbd1bf39f4db30ac2f62fd6d8
George Lebl <jirka@5z.com>
2000-08-30 23:08:51 +0000

    fix sensitivities of widgets and minor cosmetic improvements
    Wed Aug 30 16:03:55 2000  George Lebl <jirka@5z.com>
    
    	* gnome-hint-properties.c: fix sensitivities of widgets and
    	  minor cosmetic improvements
    
    svn path=/trunk/; revision=5898

--- gnome-hint/gnome-hint-properties.c
+++ gnome-hint/gnome-hint-properties.c
@@ -26,23 +26,22 @@
 static gboolean changing = FALSE;
 
 static void
-changed_cb(GtkWidget *button, GtkWidget *capplet)
+changed_cb(GtkWidget *button, GtkWidget *crapplet)
 {
-	if(changing)
-		return;
-	capplet_widget_state_changed(CAPPLET_WIDGET(capplet), TRUE);
+	if( ! changing)
+		capplet_widget_state_changed(CAPPLET_WIDGET(crapplet), TRUE);
 }
 
 static void
 cb_enable_cb(GtkWidget *button, GtkWidget *enable)
 {
-	gtk_widget_set_sensitive(enable,GTK_TOGGLE_BUTTON(button)->active);
+	gtk_widget_set_sensitive(enable, GTK_TOGGLE_BUTTON(button)->active);
 }
 
 /*FIXME: we need some help for this I guess*/
 #if 0
 static void
-help(GtkWidget *capplet)
+help(GtkWidget *crapplet)
 {
   gchar *tmp, *helpfile;
 
@@ -85,7 +84,7 @@ loadup_vals(void)
 }
 
 static void
-try(GtkWidget *capplet, gpointer data)
+try(GtkWidget *crapplet, gpointer data)
 {
 	gnome_config_set_bool("/Gnome/Login/RunHints",
 			      GTK_TOGGLE_BUTTON(enable_box)->active);
@@ -99,7 +98,7 @@ try(GtkWidget *capplet, gpointer data)
 }
 
 static void
-revert(GtkWidget *capplet, gpointer data)
+revert(GtkWidget *crapplet, gpointer data)
 {
 	gtk_toggle_button_set_state(GTK_TOGGLE_BUTTON(enable_box),
 				    hints_enabled);
@@ -107,134 +106,147 @@ revert(GtkWidget *capplet, gpointer data)
 				    display_fortune);
 	gtk_toggle_button_set_state(GTK_TOGGLE_BUTTON(motd_box),
 				    display_motd);
-	gtk_entry_set_text(GTK_ENTRY(motd_entry),motdfile);
-	try(capplet,data);
+	gtk_entry_set_text(GTK_ENTRY(motd_entry), motdfile);
+	try(crapplet, data);
 }
 
 static void
-setup_the_ui(GtkWidget *the_capplet)
+setup_the_ui(GtkWidget *the_crapplet)
 {
 	GtkWidget *vbox;
 	GtkWidget *hbox;
-	GtkWidget *w,*e,*l;
+	GtkWidget *hints_box;
+	GtkWidget *motd_gentry, *motd_label;
 
-	gtk_container_set_border_width(GTK_CONTAINER(the_capplet), GNOME_PAD);
+	gtk_container_set_border_width(GTK_CONTAINER(the_crapplet), GNOME_PAD);
 	
-	vbox = gtk_vbox_new(FALSE,GNOME_PAD_SMALL);
-	gtk_container_add(GTK_CONTAINER(the_capplet), vbox);
+	vbox = gtk_vbox_new(FALSE, GNOME_PAD_SMALL);
+	gtk_container_add(GTK_CONTAINER(the_crapplet), vbox);
 	
 	/*add the enable box*/
 	enable_box = gtk_check_button_new_with_label(_("Enable login hints"));
-	gtk_box_pack_start(GTK_BOX(vbox),enable_box,FALSE,FALSE,0);
-	gtk_signal_connect(GTK_OBJECT(enable_box),"toggled",
+	gtk_box_pack_start(GTK_BOX(vbox), enable_box, FALSE, FALSE, 0);
+	gtk_signal_connect(GTK_OBJECT(enable_box), "toggled",
 			   GTK_SIGNAL_FUNC(changed_cb),
-			   the_capplet);
+			   the_crapplet);
 
 	/*add the hint box*/
-	w = gtk_radio_button_new_with_label(NULL,_("Display normal hints"));
-	gtk_box_pack_start(GTK_BOX(vbox),w,FALSE,FALSE,0);
-	gtk_signal_connect(GTK_OBJECT(w),"toggled",
+	hints_box = gtk_radio_button_new_with_label(NULL, _("Display normal hints"));
+	gtk_box_pack_start(GTK_BOX(vbox), hints_box, FALSE, FALSE, 0);
+	gtk_signal_connect(GTK_OBJECT(hints_box), "toggled",
 			   GTK_SIGNAL_FUNC(changed_cb),
-			   the_capplet);
+			   the_crapplet);
 
 	/*add the fortune box*/
 	fortune_box = gtk_radio_button_new_with_label(
-		gtk_radio_button_group(GTK_RADIO_BUTTON(w)),
+		gtk_radio_button_group(GTK_RADIO_BUTTON(hints_box)),
 		_("Display fortunes instead of hints"));
-	gtk_box_pack_start(GTK_BOX(vbox),fortune_box,FALSE,FALSE,0);
-	gtk_signal_connect(GTK_OBJECT(fortune_box),"toggled",
+	gtk_box_pack_start(GTK_BOX(vbox), fortune_box, FALSE, FALSE, 0);
+	gtk_signal_connect(GTK_OBJECT(fortune_box), "toggled",
 			   GTK_SIGNAL_FUNC(changed_cb),
-			   the_capplet);
+			   the_crapplet);
 
 	/*add the motd box*/
 	motd_box = gtk_radio_button_new_with_label(
-		gtk_radio_button_group(GTK_RADIO_BUTTON(w)),
+		gtk_radio_button_group(GTK_RADIO_BUTTON(hints_box)),
 		_("Display message of the day instead of hints"));
-	gtk_box_pack_start(GTK_BOX(vbox),motd_box,FALSE,FALSE,0);
-	gtk_signal_connect(GTK_OBJECT(motd_box),"toggled",
+	gtk_box_pack_start(GTK_BOX(vbox), motd_box, FALSE, FALSE, 0);
+	gtk_signal_connect(GTK_OBJECT(motd_box), "toggled",
 			   GTK_SIGNAL_FUNC(changed_cb),
-			   the_capplet);
+			   the_crapplet);
 
 	/*the motd entry*/
-	hbox = gtk_hbox_new(FALSE,GNOME_PAD_SMALL);
-	gtk_box_pack_start(GTK_BOX(vbox),hbox,FALSE,FALSE,0);
-	l = gtk_label_new(_("Message of the day file to use: ")),
-	gtk_box_pack_start(GTK_BOX(hbox), l, FALSE,FALSE,0);
-	e = gnome_file_entry_new("motd",_("Browse"));
-	gtk_box_pack_start(GTK_BOX(hbox),e,TRUE,TRUE,0);
-	motd_entry = gnome_file_entry_gtk_entry(GNOME_FILE_ENTRY(e));
-	gtk_signal_connect(GTK_OBJECT(motd_entry),"changed",
+	hbox = gtk_hbox_new(FALSE, GNOME_PAD_SMALL);
+	gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
+	motd_label = gtk_label_new(_("Message of the day file to use: ")),
+	gtk_box_pack_start(GTK_BOX(hbox), motd_label, FALSE, FALSE, 0);
+	motd_gentry = gnome_file_entry_new("motd", _("Browse"));
+	gtk_box_pack_start(GTK_BOX(hbox), motd_gentry, TRUE, TRUE, 0);
+	motd_entry = gnome_file_entry_gtk_entry(GNOME_FILE_ENTRY(motd_gentry));
+	gtk_signal_connect(GTK_OBJECT(motd_entry), "changed",
 			   GTK_SIGNAL_FUNC(changed_cb),
-			   the_capplet);
+			   the_crapplet);
 
 	/*here we set up the enable disable thing*/
-	gtk_signal_connect(GTK_OBJECT(enable_box),"toggled",
+	gtk_signal_connect(GTK_OBJECT(enable_box), "toggled",
 			   GTK_SIGNAL_FUNC(cb_enable_cb),
-			   w);
-	gtk_signal_connect(GTK_OBJECT(enable_box),"toggled",
+			   hints_box);
+	gtk_signal_connect(GTK_OBJECT(enable_box), "toggled",
 			   GTK_SIGNAL_FUNC(cb_enable_cb),
 			   fortune_box);
-	gtk_signal_connect(GTK_OBJECT(enable_box),"toggled",
+	gtk_signal_connect(GTK_OBJECT(enable_box), "toggled",
 			   GTK_SIGNAL_FUNC(cb_enable_cb),
 			   motd_box);
 	/*!!!we need to use different widgets for enable box and motd_box*/
-	gtk_signal_connect(GTK_OBJECT(enable_box),"toggled",
+	gtk_signal_connect(GTK_OBJECT(enable_box), "toggled",
 			   GTK_SIGNAL_FUNC(cb_enable_cb),
 			   hbox);
-	gtk_signal_connect(GTK_OBJECT(motd_box),"toggled",
+	gtk_signal_connect(GTK_OBJECT(motd_box), "toggled",
 			   GTK_SIGNAL_FUNC(cb_enable_cb),
-			   e);
-	gtk_signal_connect(GTK_OBJECT(motd_box),"toggled",
+			   motd_gentry);
+	gtk_signal_connect(GTK_OBJECT(motd_box), "toggled",
 			   GTK_SIGNAL_FUNC(cb_enable_cb),
-			   l);
-	
-
-
+			   motd_label);
 
 	/*setup defaults*/
 	gtk_toggle_button_set_state(GTK_TOGGLE_BUTTON(enable_box),
 				    hints_enabled);
+	gtk_toggle_button_set_state(GTK_TOGGLE_BUTTON(hints_box),
+				    ! display_fortune &&
+				    ! display_motd);
 	gtk_toggle_button_set_state(GTK_TOGGLE_BUTTON(fortune_box),
 				    display_fortune);
 	gtk_toggle_button_set_state(GTK_TOGGLE_BUTTON(motd_box),
 				    display_motd);
-	gtk_entry_set_text(GTK_ENTRY(motd_entry),motdfile);
+	gtk_entry_set_text(GTK_ENTRY(motd_entry), motdfile);
+
+	/* Set up default sensitivities */
+	gtk_widget_set_sensitive (hbox, hints_enabled);
+	gtk_widget_set_sensitive (hints_box, hints_enabled);
+	gtk_widget_set_sensitive (fortune_box, hints_enabled);
+	gtk_widget_set_sensitive (motd_box, hints_enabled);
+
+	gtk_widget_set_sensitive (motd_gentry, display_motd);
+	gtk_widget_set_sensitive (motd_label, display_motd);
 
 	/* Finished */
-	gtk_widget_show_all(the_capplet);
-	gtk_signal_connect(GTK_OBJECT(the_capplet), "try",
+	gtk_widget_show_all(the_crapplet);
+	gtk_signal_connect(GTK_OBJECT(the_crapplet), "try",
 			   GTK_SIGNAL_FUNC(try), NULL);
-	gtk_signal_connect(GTK_OBJECT(the_capplet), "revert",
+	gtk_signal_connect(GTK_OBJECT(the_crapplet), "revert",
 			   GTK_SIGNAL_FUNC(revert), NULL);
-	gtk_signal_connect(GTK_OBJECT(the_capplet), "ok",
+	gtk_signal_connect(GTK_OBJECT(the_crapplet), "ok",
 			   GTK_SIGNAL_FUNC(try), NULL);
-	gtk_signal_connect(GTK_OBJECT(the_capplet), "cancel",
+	gtk_signal_connect(GTK_OBJECT(the_crapplet), "cancel",
 			   GTK_SIGNAL_FUNC(revert), NULL);
-	/*gtk_signal_connect(GTK_OBJECT(the_capplet), "help",
+	/*gtk_signal_connect(GTK_OBJECT(the_crapplet), "help",
 			   GTK_SIGNAL_FUNC(help), NULL);*/
 }
 
 int
 main (int argc, char **argv)
 {
-	GtkWidget *the_capplet;
+	GtkWidget *the_crapplet;
 
 	bindtextdomain(PACKAGE, GNOMELOCALEDIR);
 	textdomain(PACKAGE);
 
+	/* Make sure that we don't register changes yet */
 	changing = TRUE;
 
 	if(gnome_capplet_init("gnome-hint-properties", VERSION, argc,
 			      argv, NULL, 0, NULL) < 0)
 		return 1;
-	gnome_window_icon_set_default_from_file (GNOME_ICONDIR"/gnome-hint.png");
+	gnome_window_icon_set_default_from_file (GNOME_ICONDIR "/gnome-hint.png");
 	loadup_vals();
 
-	the_capplet = capplet_widget_new();
+	the_crapplet = capplet_widget_new();
 
-	setup_the_ui(the_capplet);
+	setup_the_ui(the_crapplet);
 
+	/* start registering user changes */
 	changing = FALSE;
+
 	capplet_gtk_main();
 
 	return 0;
